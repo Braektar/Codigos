aa3979f96f9bd7dacb8a3993ee77563003eebe44
Braektar <carlosfeliperomocarmona@gmail.com>
2026-07-23 00:12:44 -0400

    Finalizado menús iniciales:
    -Menu principal
    -Menu usuario
    -Menu admin

---
+++ T0/Codigo/menu.py
@@ -0,0 +1,26 @@
+def Inicio():
+    print("** Menú de Inicio **\n")
+    print("Selecciona una de las siguientes opciones:\n \n")
+    print("[1] Iniciar sesión como usuario")
+    print("[2] Registrarse como usuario")
+    print("[3] Iniciar sesión como administrador")
+    print("[4] Salir del programa \n")
+
+    return()
+
+def Usuario():
+    print("** Menú de usuario **\n")
+    print("[1] Hacer encomienda")
+    print("[2] Revisar estado de encomiendas realizadas")
+    print("[3] Realizar reclamos")
+    print("[4] Ver el estado de los pedidos personales")
+    print("[5] Cerrar sesión\n")
+
+    return()
+
+def Admin():
+    print("** Menú de administrador **\n")
+    print("[1] Actualizar encomiendas")
+    print("[2] Revisar reclamos")
+    print("[3] Cerrar sesión\n")
+    return()
